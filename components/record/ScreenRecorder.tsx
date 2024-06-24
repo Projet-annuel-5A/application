518447d6797a826d2934c68058ec0c6f19bde90f
Collas Pierre <collaspierre51@gmail.com>
2024-06-24 20:18:09 +0200

    enable to click two times on the send button

--- components/record/ScreenRecorder.tsx
+++ components/record/ScreenRecorder.tsx
@@ -1,100 +1,3 @@
-// "use client"
-// import React, { useState } from 'react';
-// import { createClient } from '@/utils/supabase/client';
-// import { Session, Interview } from '@/app/types/database';
-// import { usePathname, useRouter } from 'next/navigation';
-
-// export default function ScreenRecorder({ sessionID, interviewID }: { sessionID: Session["id"], interviewID: Interview["id"] }) {
-//     const supabase = createClient();
-//     const path = usePathname();
-//     const router = usePathname();
-//     const [recording, setRecording] = useState(false);
-//     const [mediaRecorder, setMediaRecorder] = useState<any>(null);
-//     const [showSaveButton, setShowSaveButton] = useState(false);
-//     const [recordedBlob, setRecordedBlob] = useState(null);
-
-//     const startRecording = async () => {
-//         try {
-//             const stream = await navigator.mediaDevices.getDisplayMedia({ video: true });
-//             const recorder = new MediaRecorder(stream) as any;
-
-//             recorder.ondataavailable = (e: any) => {
-//                 setRecordedBlob(e.data);
-//                 setShowSaveButton(true);
-//             };
-
-//             recorder.start();
-//             setRecording(true);
-//             setMediaRecorder(recorder);
-//         } catch (error) {
-//             console.error("Erreur lors du démarrage de l'enregistrement de l'écran", error);
-//         }
-//     };
-
-//     const stopRecording = () => {
-//         if (!mediaRecorder) return;
-//         mediaRecorder.stop();
-//         setRecording(false);
-//     };
-
-//     const saveRecording = async () => {
-//         if (!recordedBlob) return;
-
-//         // const file = new File([recordedBlob], 'screenRecording.webm', { type: recordedBlob.type });
-//         const file = new File([recordedBlob], 'screenRecording.mp4', { type: 'video/mp4' });
-//         // const { data, error } = await supabase.storage.from('interviews').upload(`interviews/${sessionID}/${interviewID}/raw/raw.webm`, file);
-//         const { error } = await supabase.storage.from('interviews').upload(`/interviews/${sessionID}/${interviewID}/raw/raw.mp4`, file);
-
-//         if (error) {
-//             console.error("Erreur lors de l'upload de l'enregistrement", error);
-//         } else {
-//             console.log("Enregistrement sauvegardé avec succès:");
-//             await supabase.from("interviews").update({ 'raw_file_ok': true }).filter('id', 'eq', interviewID);
-//             // TODO: USE REAL CLOUD API URL 
-//             try {
-//                 fetch(`http://127.0.0.1:8000/predict`, {
-//                     method: 'POST',
-//                     headers: {
-//                         'Content-Type': 'application/json'
-//                     },
-//                     body: JSON.stringify({
-//                         session_id: Number(sessionID),
-//                         interview_id: Number(interviewID)
-//                     })
-//                 });
-//             } catch (error) {
-//                 console.error('error starting video processing', error)
-//             }
-
-//         }
-//     };
-
-//     return (
-//         <div>
-//             <div className='flex justify-center'>
-//                 <button
-//                     onClick={!recording ? startRecording : stopRecording}
-//                     className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-//                 >
-//                     {!recording ? 'Start recording' : 'Stop recording'}
-//                 </button>
-//                 <p className="mx-2 text-white font-extrabold flex items-center h-full"> or</p>
-//                 {showSaveButton && (
-//                     <button
-//                         onClick={saveRecording}
-//                         className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-//                     >
-//                         Save Interview
-//                     </button>
-//                 )}
-//             </div>
-
-
-//         </div>
-//     );
-// }
-
-
 "use client"
 import React, { useState } from 'react';
 import { createClient } from '@/utils/supabase/client';
@@ -108,6 +11,7 @@ export default function ScreenRecorder({ sessionID, interviewID }: { sessionID: 
     const [mediaRecorder, setMediaRecorder] = useState<any>(null);
     const [showSaveButton, setShowSaveButton] = useState(false);
     const [recordedBlob, setRecordedBlob] = useState<Blob | null>(null);
+    const [isSaving, setIsSaving] = useState(false);
 
     const startRecording = async () => {
         try {
@@ -144,6 +48,8 @@ export default function ScreenRecorder({ sessionID, interviewID }: { sessionID: 
     const saveRecording = async () => {
         if (!recordedBlob) return;
 
+        setIsSaving(true);
+
         const file = new File([recordedBlob], 'screenRecording.mp4', { type: 'video/mp4' });
         const { error } = await supabase.storage.from('interviews').upload(`${sessionID}/${interviewID}/raw/raw.mp4`, file);
 
@@ -153,7 +59,12 @@ export default function ScreenRecorder({ sessionID, interviewID }: { sessionID: 
             console.log("Enregistrement sauvegardé avec succès:");
             await supabase.from("interviews").update({ 'raw_file_ok': true }).filter('id', 'eq', interviewID);
             try {
-                fetch(`http://${process.env.NEXT_MIDDLEWARE_IP}/predict`, {
+                if (process.env.NEXT_PUBLIC_ENV === 'production'){
+                    var url = `https://${process.env.NEXT_PUBLIC_MIDDLEWARE_IP}/predict`;
+                } else {
+                    var url = `http://${process.env.NEXT_PUBLIC_MIDDLEWARE_IP}:8000/predict`;
+                }
+                fetch(url, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
@@ -168,6 +79,7 @@ export default function ScreenRecorder({ sessionID, interviewID }: { sessionID: 
                 console.error('error starting video processing', error)
             }
         }
+        setIsSaving(false);
     };
 
     return (
@@ -197,6 +109,7 @@ export default function ScreenRecorder({ sessionID, interviewID }: { sessionID: 
                     <button
                         onClick={saveRecording}
                         className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mx-4"
+                        disabled={isSaving}
                     >
                         Save Interview
                     </button>
